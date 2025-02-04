2129afbf8c1e7332ef2094ac5d9dec27210bd332
mohamedamr77 <moamr947@gmail.com>
2025-02-04 03:12:59 +0200

    make entitiy

---
+++ lib/Features/home/domain/entities/book_entity.dart
@@ -0,0 +1,8 @@
+class BookEntity{
+  final String image;
+  final String name;
+  final String description;
+  final num price;
+  final num rate ;
+  BookEntity({required this.image, required this.name, required this.description, required this.price, required this.rate});
+}
